02a6aeadde882249313e62b11cb67853a7632594
Raghavendra <vikramnbhat15@gmail.com>
2026-01-26 19:44:30 +0530

    fix: change node module resolution in tsconfig

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,6 +4,6 @@
  * This module exports all service classes and instances for easy importing
  */
 
-export { ResendService, resendService, type CreateTemplateParams, type UpdateTemplateParams, type ListTemplatesParams, type SendEmailParams, type TemplateVariable } from './resend';
-export { EmailService, emailService, type SendOTPEmailParams, type SendInvitationEmailParams } from './email';
-export { TemplateManager, templateManager, TEMPLATE_IDS } from './templates';
+export { ResendService, resendService, type CreateTemplateParams, type UpdateTemplateParams, type ListTemplatesParams, type SendEmailParams, type TemplateVariable } from './resend.js';
+export { EmailService, emailService, type SendOTPEmailParams, type SendInvitationEmailParams } from './email.js';
+export { TemplateManager, templateManager, TEMPLATE_IDS } from './templates.js';
