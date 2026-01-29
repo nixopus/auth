bb34b37c071ecac12c9460d77620beafc717ec28
raghavyuva <vikramnbhat15@gmail.com>
2026-01-29 19:25:53 +0530

    fix: add migrations for billing

---
+++ drizzle/0008_stormy_doctor_doom.sql
@@ -0,0 +1,6 @@
+DROP TABLE "billing_accounts" CASCADE;--> statement-breakpoint
+DROP TABLE "invoices" CASCADE;--> statement-breakpoint
+DROP TABLE "payment_events" CASCADE;--> statement-breakpoint
+DROP TABLE "subscriptions" CASCADE;--> statement-breakpoint
+DROP TYPE "public"."invoice_status";--> statement-breakpoint
+DROP TYPE "public"."subscription_status";
